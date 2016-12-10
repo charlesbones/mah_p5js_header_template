5f6fb9ec6f39b7ae58cc853ab406909394f3f4af
Charles Bones <bones.charles@gmail.com>
2016-12-10 23:57:56 +0100

    first push with html, css and one js

---
+++ background.js
@@ -0,0 +1,63 @@
+var x=0;
+var y=0;
+var a=new arrowX();
+var divHeader;
+var bodySize;
+var authorName="Mario Rodriguez";
+var authorSite="bones.sikuani.net";
+function setup() {
+  canvasSetup();
+  authorInfo();
+  background(200);
+}
+
+function draw() {
+  fill(200,10);
+  rect(0,0,width,height);
+  for (var i=0; i<100; i++) {
+    a.render();
+  }
+}
+
+function arrowX() {
+  this.x=0;
+  this.y=100;
+  this.render=function() {
+    stroke(255);
+    point(this.x, this.y);
+    this.x++;
+    if (this.x>width) {
+      this.x=0;
+      this.y=random(0, height);
+    }
+  }
+}
+/*-----------
+Author's Information formating function
+PLEASE DON'T MODIFY THIS FUNCTION
+-----------*/
+function authorInfo(){
+  var author= createP(authorName);
+  var authorContact= createA(authorSite,authorSite);
+  author.style("position","absolute");
+  author.style("right","1%");
+  author.style("bottom",divHeader.height/100+'%');
+  author.style("font-size","80%");
+  author.parent("p5Gallery");
+  authorContact.style("position","absolute");
+  authorContact.style("right","1%");
+  authorContact.style("bottom",divHeader.height/100+'%');
+  authorContact.style("font-size","70%");
+  authorContact.parent("p5Gallery");
+}
+function canvasSetup(){
+  divHeader=select('#masthead');
+  bodySize=select('body');
+  var canvas= createCanvas(bodySize.width , divHeader.height);
+  canvas.parent("p5Gallery");
+}
+function windowResized(){
+  divHeader=select('#masthead');
+  bodySize=select('body');
+  resizeCanvas(bodySize.width, divHeader.height);
+}
